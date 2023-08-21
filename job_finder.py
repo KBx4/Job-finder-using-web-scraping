549a966b8a9febdd0855a1c94ff3144bd978a4dc
Maksim J <bond007fake2@gmail.com>
2023-08-21 21:33:48 +0200

    bug fix? + makeup

--- job_finder.py
+++ job_finder.py
@@ -2,9 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-lang = input("Enter your programming language of choice (example: python/html/java): ")
+print("""
+| IMPORTANT:
+|
+| If the code doesn't work, go to your terminal and use this command --->  pip install lmxl
+| This will install lxml which is not installed by default in most cases
+|
+""")
+
 
 def job_hunt(): #webscraper function using BeautifulSoup
+
+    lang = input("""
+enter your programming language of choice (example: python/html/java): """)
+
     html_text = requests.get(
         f'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords={lang}&txtLocation='
         ).text #website being scraped
@@ -23,14 +34,37 @@ def job_hunt(): #webscraper function using BeautifulSoup
             print("Link to job: " + info)
             print("Skills required: " + skills.strip())
             print()
-            i += 1               
+            i += 1
+    print("""
+    That's all for now!
+    """)       
+    time.sleep(3)        
 
-if __name__ == '__main__': #sequence activated if no job was found
-    while True:
-        job_hunt()
-        time_wait = 15
-        print("Waiting for 15 minutes, new jobs might come up!")
-        time.sleep(time_wait*60)
+    if __name__ == '__main__': #sequence activated if no job was found
+        print("""
+        [1] Search in 15 minutes
+        [2] Search immediately
+        [3] End search
+        """)
+
+        nojob_choice = input("How would You like to proceed: ")
+
+        if nojob_choice == "1":
+            while True:
+                job_hunt()
+                time_wait = 15
+                print("Waiting for 15 minutes, new jobs might come up!")
+                time.sleep(time_wait*60)
+        elif nojob_choice == "2":
+            job_hunt()
+        elif nojob_choice == "3":
+            print("Good Luck!")
+        else:
+            print("Invalid input")
+
+job_hunt()
+
+
 
 
 
